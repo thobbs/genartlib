32fd30f1c633603fbd458fa99332277af4c5268b
Tyler Hobbs <tyler@tylerlhobbs.com>
2019-03-27 21:52:55 -0500

    Update genartlib dep in project template

--- project-template/project.clj
+++ project-template/project.clj
@@ -1,9 +1,7 @@
 (defproject sketch "0.1.0-SNAPSHOT"
   :description "A template for a sketch with Quil/Processing"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.apache.commons/commons-math3 "3.3"]
-                 [quil "2.7.1"]
-                 [genartlib "0.1.17"] ; utility functions
+                 [genartlib/genartlib "0.1.18"] ; utility functions
                  [com.seisw/gpcj "2.2.0"]] ; polygon clipping
   :jvm-opts ["-Xms4000m" "-Xmx4000M" "-server"] ; 4GB heap size
   :source-paths ["src/clj"]
