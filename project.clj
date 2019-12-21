06897315340dea5454af8ad0555937c26adde373
Tyler Hobbs <tyler@tylerlhobbs.com>
2019-12-21 14:53:16 -0600

    Release 0.1.19

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject genartlib "0.1.18-SNAPSHOT"
+(defproject genartlib "0.1.19"
   :description "Utilities and common tasks for generative artwork"
   :url "http://github.com/thobbs/genartlib"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.apache.commons/commons-math3 "3.3"]
                  [quil "2.7.1"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
-  :aot [genartlib.geometry genartlib.util genartlib.random genartlib.algebra genartlib.curves])
+  :aot [genartlib.geometry genartlib.util genartlib.random genartlib.algebra genartlib.curves genartlib.plotter])
