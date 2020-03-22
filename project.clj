cbbe587dc21de4a5f1d11d529fd2377474fd0f3d
Tyler Hobbs <tyler@tylerlhobbs.com>
2020-03-22 16:47:10 -0500

    Release 0.1.20

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject genartlib "0.1.20-SNAPSHOT"
+(defproject genartlib "0.1.20"
   :description "Utilities and common tasks for generative artwork"
   :url "http://github.com/thobbs/genartlib"
   :license {:name "MIT"
