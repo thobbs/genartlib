931751325d977b9bd517fccf60740a8417d16c27
Tyler Hobbs <tyler@tylerlhobbs.com>
2020-03-22 14:17:28 -0500

    Upgrade Quil/Processing deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/thobbs/genartlib"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.apache.commons/commons-math3 "3.3"]
-                 [quil "2.7.1"]]
+                 [quil "3.1.0"]]
   :deploy-repositories [["releases" {:sign-releases false :url "https://clojars.org"}]
                         ["snapshots" {:sign-releases false :url "https://clojars.org"}]]
   :source-paths ["src/clj"]
