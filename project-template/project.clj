9d6632e91caf538b8eaf5d4943e7a66a4c953392
Tyler Hobbs <tyler@tylerlhobbs.com>
2020-10-17 17:10:54 -0500

    Make it easy to scale display for high DPI

--- project-template/project.clj
+++ project-template/project.clj
@@ -3,7 +3,9 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [genartlib/genartlib "0.1.20"] ; utility functions
                  [com.seisw/gpcj "2.2.0"]] ; polygon clipping
-  :jvm-opts ["-Xms4000m" "-Xmx4000M" "-server"] ; 4GB heap size
+  :jvm-opts ["-Xms4000m" "-Xmx4000M" ; 4GB heap size
+             "-server"
+             "-Dsun.java2d.uiScale=1.0"] ; adjust scaling for high DPI displays
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :aot [sketch.dynamic])
