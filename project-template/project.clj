df01a5ec9ce1883c69bef07c020d6c838aef923f
Tyler Hobbs <tyler@tylerlhobbs.com>
2020-11-16 22:04:54 -0600

    Remove gpcj from project template deps

--- project-template/project.clj
+++ project-template/project.clj
@@ -1,8 +1,7 @@
 (defproject sketch "0.1.0-SNAPSHOT"
   :description "A template for a sketch with Quil/Processing"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [genartlib/genartlib "0.1.20"] ; utility functions
-                 [com.seisw/gpcj "2.2.0"]] ; polygon clipping
+                 [genartlib/genartlib "0.1.20"]] ; utility functions
   :jvm-opts ["-Xms4000m" "-Xmx4000M" ; 4GB heap size
              "-server"
              "-Dsun.java2d.uiScale=1.0"] ; adjust scaling for high DPI displays
