784888d102081b48b932dba5fd07e37b4e916b18
Tyler Hobbs <tyler@tylerlhobbs.com>
2024-06-25 11:18:40 -0500

    Upgrade quil and clojure deps

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject genartlib "1.0.1-SNAPSHOT"
+(defproject genartlib "1.1.0-SNAPSHOT"
   :description "Utilities and common tasks for generative artwork"
   :url "http://github.com/thobbs/genartlib"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
                  [org.apache.commons/commons-math3 "3.3"]
-                 [quil "4.3.1323"]]
+                 [quil "4.3.1563"]]
   :deploy-repositories [["clojars" {:sign-releases false}]
                         ["releases" {:sign-releases false :url "https://clojars.org"}]
                         ["snapshots" {:sign-releases false :url "https://clojars.org"}]]
