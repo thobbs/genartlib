64811498bce81189b67752f012bf9ca6ca743d71
Tyler Hobbs <tyler@tylerlhobbs.com>
2024-09-09 13:23:57 -0500

    Release version 1.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject genartlib "1.1.1-SNAPSHOT"
+(defproject genartlib "1.2.0"
   :description "Utilities and common tasks for generative artwork"
   :url "http://github.com/thobbs/genartlib"
   :license {:name "MIT"
